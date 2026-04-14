856d8b8cc8461f371ffd8a822d2f1b6180a30e03
vladislaav <parieiko.vlad@gmail.com>
2026-04-14 23:02:17 +0400

    practicum 6

--- projects/models/project_file.py
+++ projects/models/project_file.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class ProjectFile(models.Model):
-    name = models.CharField(unique=True, max_length=120)
+    name = models.CharField(max_length=120)
     file = models.CharField(max_length=255)
     created_at = models.DateField(
         auto_now_add=True
